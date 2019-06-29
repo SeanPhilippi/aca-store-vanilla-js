e126ded650f6f07e610198576b0a0f536e7c541d
AFutureWebDev <jenna.gullick2@gmail.com>
2019-06-29 16:07:13 -0500

    iniitial commit

--- public/index.js
+++ public/index.js
@@ -1,1 +1,20 @@
 //stuff
+
+function Products(products) {
+let productDiv = "";
+for(let i =0; i < products.length; i++) {
+        let product = products[i];
+        productDiv += `<div>${product.name}</div>`
+}
+document.getElementById("products").innerHTML= productDiv;
+
+}
+ Products(products);
+
+ function search() {
+     let searchWord = document.getElementById("searchBox").value;
+     let filteredProducts = products.filter(p => p.name === searchWord)
+
+    Products(filteredProducts);
+ }
+
